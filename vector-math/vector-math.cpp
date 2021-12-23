f3bbb7eec88ef0b8647417d169268eb3c7c85eab
Nikita Elsakov <vocasle@gmailcom>
2021-12-23 22:41:03 +0300

    Add example of floating point error

--- vector-math/vector-math.cpp
+++ vector-math/vector-math.cpp
@@ -6,13 +6,9 @@
 
 using namespace DirectX;
 
-std::ostream& operator<<(std::ostream& out, FXMVECTOR v)
-{
-    XMFLOAT3 dest;
-    XMStoreFloat3(&dest, v);
-    out << "(" << dest.x << "," << dest.y << "," << dest.z << ")";
-    return out;
-}
+std::ostream& operator<<(std::ostream& out, FXMVECTOR v);
+
+void FloatingPointErrorExample();
 
 int main()
 {
@@ -57,6 +53,7 @@ int main()
     const float angleDegrees = XMConvertToDegrees(angleRadians);
 
     std::cout.setf(std::ios_base::boolalpha);
+    std::cout << "***DirectXMath vector operations***\n" << std::endl;
     std::cout << "u = " << u << std::endl;
     std::cout << "v = " << v << std::endl;
     std::cout << "w = " << w << std::endl;
@@ -73,4 +70,34 @@ int main()
     std::cout << "projW + perpW == w = " << equal << std::endl;
     std::cout << "projW + perpW != w = " << notEqual << std::endl;
     std::cout << "angle = " << angleDegrees << std::endl;
+
+    std::cout << "\n\n\n" << std::endl;
+    FloatingPointErrorExample();
+}
+
+void FloatingPointErrorExample()
+{
+    std::cout << "***Floating point error***\n" << std::endl;
+    std::cout.precision(8);
+
+    const XMVECTOR u = XMVectorSet(1.0f, 1.0f, 1.0f, 0.0f);
+    const XMVECTOR n = XMVector3Normalize(u);
+    const float lu = XMVectorGetX(XMVector3Length(n));
+
+    std::cout << "Mathematically, the length should be 1. Is it numerically?" << std::endl;
+    std::cout << lu << std::endl;
+    std::cout << (lu == 1.0f ? "length 1" : "length not 1") << std::endl;
+
+    std::cout << "Raising 1 to any power should still be 1. Is it?" << std::endl;
+    const float powLU = std::powf(lu, 1.0e6f);
+    std::cout << "lu^(10^6) = " << powLU << std::endl;
+    
+}
+
+std::ostream& operator<<(std::ostream& out, FXMVECTOR v)
+{
+    XMFLOAT3 dest;
+    XMStoreFloat3(&dest, v);
+    out << "(" << dest.x << "," << dest.y << "," << dest.z << ")";
+    return out;
 }
