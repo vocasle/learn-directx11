847e5fd6643257b3ede02339dc7a30fcdb417abb
Nikita Elsakov <vocasle@gmailcom>
2022-01-18 10:22:24 +0300

    Add constant buffer to vertex shader
    I supply cbuffer to perform a world view projection multiplication inside a vertex shader

--- cube/VertexShader.hlsl
+++ cube/VertexShader.hlsl
@@ -10,7 +10,18 @@ struct Interpolants
     float4 color        : COLOR0;
 };
 
+cbuffer Constants : register(b0)
+{
+    float4x4 mWorld;
+    float4x4 mView;
+    float4x4 mProjection;
+};
+
 Interpolants main(Vertex In)
 {
-    return In;
+    Interpolants Out = In;
+    Out.position = mul(Out.position, mWorld);
+    Out.position = mul(Out.position, mView);
+    Out.position = mul(Out.position, mProjection);
+    return Out;
 }
