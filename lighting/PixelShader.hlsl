86ed24168229b527ae4cb4103cff408e8566634a
Nikita Elsakov <vocasle@outlook.com>
2022-02-09 11:15:21 +0300

    Surface point must be in World space

--- lighting/PixelShader.hlsl
+++ lighting/PixelShader.hlsl
@@ -52,11 +52,12 @@ cbuffer LightingData
     Material material;
 };
 
-struct Interpolants
+struct PSInput
 {
     float4 position : SV_Position;
     float4 color    : COLOR0;
     float3 normal   : NORMAL;
+    float3 positionW : POSITION;
 };
 
 struct Pixel
@@ -92,23 +93,87 @@ void ComputeDirectionalLight(Material mat,
     }
 }
 
-Pixel main(Interpolants In)
+void ComputePointlLight(Material mat,
+    PointLight pl,
+    float3 normal,
+    float3 pos,
+    float3 toEye,
+    out float4 ambient,
+    out float4 diffuse,
+    out float4 specular)
+{
+    ambient = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    diffuse = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    specular = float4(0.0f, 0.0f, 0.0f, 0.0f);
+
+    // The vector from the surface to the light
+    float3 lightVec = pl.Position.xyz - pos;
+    float d = length(lightVec);
+
+    if (d > pl.Range)
+        return;
+
+    lightVec = normalize(lightVec);
+
+    ambient = mat.Ambient * pl.Ambient;
+
+    // Add diffuse and specular term, provided the surface is in
+    // the line of site of the light
+    float diffuseFactor = dot(lightVec, normal);
+
+    if (diffuseFactor > 0.0f)
+    {
+        float3 v = reflect(-lightVec, normal);
+        float specFactor = pow(max(dot(v, toEye), 0.0f), mat.Specular.w);
+
+        diffuse = diffuseFactor * mat.Diffuse * pl.Diffuse;
+        specular = specFactor * mat.Specular * pl.Specular;
+    }
+
+    // attenuate
+    float att = 1.0f / dot(pl.Att, float3(1.0f, d, d * d));
+
+    diffuse *= att;
+    specular *= att;
+}
+
+Pixel main(PSInput In)
 {
     Pixel Out;
-    //Out.color = In.color;
-    float4 ambient;
-    float4 diffuse;
-    float4 specular;
+    float4 ambient = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    float4 diffuse = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    float4 specular = float4(0.0f, 0.0f, 0.0f, 0.0f);
+
+    float4 A = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    float4 D = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    float4 S = float4(0.0f, 0.0f, 0.0f, 0.0f);
 
     // Interpolated normal could be unnormalized
     In.normal = normalize(In.normal);
-    float3 toEye = normalize(eyePos - float3(In.position.x, In.position.y, In.position.z));
+    float3 toEye = normalize(eyePos - float3(In.positionW.x, In.positionW.y, In.positionW.z));
 
     ComputeDirectionalLight(material, dirLight, In.normal, toEye,
-        ambient, diffuse, specular);
+        A, D, S);
+
+    ambient += A;
+    diffuse += D;
+    specular += S;
+
+    ComputePointlLight(material,
+        pointLight,
+        In.normal,
+        In.positionW,
+        toEye,
+        A,
+        D,
+        S);
+
+    ambient += A;
+    diffuse += D;
+    specular += S;
 
     Out.color = ambient + diffuse + specular;
-    //Out.color = float4(1.0f, 0.0f, 0.0f, 1.0f);
+    Out.color.w = material.Diffuse.w;
 
     return Out;
 }
