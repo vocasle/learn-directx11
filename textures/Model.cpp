2a0ff748e1775b5e021244590b4719946f434f98
Nikita Elsakov <vocasle@outlook.com>
2022-02-13 12:31:05 +0300

    Load texture from aiMaterial

--- textures/Model.cpp
+++ textures/Model.cpp
@@ -8,11 +8,69 @@
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
+#include <wincodec.h>
+
 Model::Model():
 	m_positions(),
 	m_faces(),
 	m_normals()
 {
+}
+
+std::vector<uint8_t> LoadBGRAImage(const wchar_t* filename, uint32_t& width, uint32_t& height)
+{
+	using namespace Microsoft::WRL;
+	ComPtr<IWICImagingFactory> wicFactory;
+	DX::ThrowIfFailed(CoCreateInstance(CLSID_WICImagingFactory2, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&wicFactory)));
+
+	ComPtr<IWICBitmapDecoder> decoder;
+	DX::ThrowIfFailed(wicFactory->CreateDecoderFromFilename(filename, nullptr, GENERIC_READ, WICDecodeMetadataCacheOnDemand, decoder.GetAddressOf()));
+
+	ComPtr<IWICBitmapFrameDecode> frame;
+	DX::ThrowIfFailed(decoder->GetFrame(0, frame.GetAddressOf()));
+
+	DX::ThrowIfFailed(frame->GetSize(&width, &height));
+
+	WICPixelFormatGUID pixelFormat;
+	DX::ThrowIfFailed(frame->GetPixelFormat(&pixelFormat));
+
+	uint32_t rowPitch = width * sizeof(uint32_t);
+	uint32_t imageSize = rowPitch * height;
+
+	std::vector<uint8_t> image;
+	image.resize(size_t(imageSize));
+
+	if (memcmp(&pixelFormat, &GUID_WICPixelFormat32bppBGRA, sizeof(GUID)) == 0)
+	{
+		DX::ThrowIfFailed(frame->CopyPixels(nullptr, rowPitch, imageSize, reinterpret_cast<BYTE*>(image.data())));
+	}
+	else
+	{
+		ComPtr<IWICFormatConverter> formatConverter;
+		DX::ThrowIfFailed(wicFactory->CreateFormatConverter(formatConverter.GetAddressOf()));
+
+		BOOL canConvert = FALSE;
+		DX::ThrowIfFailed(formatConverter->CanConvert(pixelFormat, GUID_WICPixelFormat32bppBGRA, &canConvert));
+		if (!canConvert)
+		{
+			throw std::exception("CanConvert");
+		}
+
+		DX::ThrowIfFailed(formatConverter->Initialize(frame.Get(), GUID_WICPixelFormat32bppBGRA,
+			WICBitmapDitherTypeErrorDiffusion, nullptr, 0, WICBitmapPaletteTypeMedianCut));
+
+		DX::ThrowIfFailed(formatConverter->CopyPixels(nullptr, rowPitch, imageSize, reinterpret_cast<BYTE*>(image.data())));
+	}
+
+	return image;
+}
+
+std::wstring StrToWstr(const std::string& s)
+{
+	size_t numConverted = 0;
+	std::unique_ptr<wchar_t*> bytes = std::make_unique<wchar_t*>(new wchar_t[s.size() + 1]);
+	mbstowcs_s(&numConverted, *bytes, s.size() + 1, s.c_str(), s.size());
+	return std::wstring(*bytes);
 }
 
 std::unique_ptr<Model> ParseObjectFile(const std::string& filepath)
@@ -36,9 +94,16 @@ std::unique_ptr<Model> ParseObjectFile(const std::string& filepath)
 	std::vector<Position> positions;
 	const aiMesh* mesh = scene->mMeshes[0];
 	positions.reserve(mesh->mNumVertices);
+	std::vector<TextCoord> textCoords;
+	textCoords.reserve(mesh->mNumVertices);
+
 	for (unsigned int i = 0; i < mesh->mNumVertices; ++i)
 	{
 		positions.push_back({ mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z });
+		if (mesh->mTextureCoords[0])
+			textCoords.push_back({ mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y });
+		else
+			textCoords.push_back({ 0.0f, 0.0f });
 	}
 
 	std::vector<Normal> normals;
@@ -61,17 +126,14 @@ std::unique_ptr<Model> ParseObjectFile(const std::string& filepath)
 			mesh->mFaces[i].mIndices[1] ,
 			mesh->mFaces[i].mIndices[2] });
 	}
-	std::vector<TextCoord> textCoords;
-	if (mesh->HasTextureCoords(0))
-	{
-		textCoords.reserve(mesh->mNumVertices);
-		for (unsigned int i = 0; i < mesh->mNumVertices; ++i)
-		{
-			textCoords.push_back({ mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y });
-		}
-	}
 
-	return std::make_unique<Model>(std::move(positions), std::move(faces), std::move(normals), std::move(textCoords));
+	const aiMaterial* mat = scene->mMaterials[mesh->mMaterialIndex];
+	aiString textPath;
+	// TODO: add support for specular and normal maps
+	mat->GetTexture(aiTextureType_DIFFUSE, 0, &textPath);
+
+
+	return std::make_unique<Model>(std::move(positions), std::move(faces), std::move(normals), std::move(textCoords), textPath.C_Str());
 }
 
 std::unique_ptr<Model> Model::LoadModel(const std::string& filepath)
@@ -102,11 +164,13 @@ const std::vector<TextCoord>& Model::GetTextCoords() const
 Model::Model(std::vector<Position>&& positions,
 	std::vector<Face>&& faces, 
 	std::vector<Normal>&& normals,
-	std::vector<TextCoord>&& textCoords):
+	std::vector<TextCoord>&& textCoords,
+	const char* textPath):
 	m_positions(positions),
 	m_faces(faces),
 	m_normals(normals),
-	m_textCoords(textCoords)
+	m_textCoords(textCoords),
+	m_textPath(textPath)
 {
 }
 
@@ -115,4 +179,57 @@ Face::Face(unsigned int x, unsigned int y, unsigned int z):
 	Y(y),
 	Z(z)
 {
+}
+
+std::string FindTexture(const std::string& filename)
+{
+	// TODO: replace with real filesystem search
+	return "../assets/" + filename;
+}
+
+void Model::LoadTexture(ID3D11Device* device)
+{
+	uint32_t width = 0;
+	uint32_t height = 0;
+	const std::string fullTextPath = FindTexture(m_textPath);
+	const std::vector<uint8_t> bytes = LoadBGRAImage(StrToWstr(fullTextPath).c_str(), width, height);
+
+	D3D11_TEXTURE2D_DESC txtDesc = {};
+	txtDesc.MipLevels = txtDesc.ArraySize = 1;
+	txtDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM_SRGB; // sunset.jpg is in sRGB colorspace
+	txtDesc.SampleDesc.Count = 1;
+	txtDesc.Usage = D3D11_USAGE_IMMUTABLE;
+	txtDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+	txtDesc.Width = width;
+	txtDesc.Height = height;
+
+	D3D11_SUBRESOURCE_DATA initData = {};
+	initData.pSysMem = &bytes[0];
+	initData.SysMemPitch = txtDesc.Width * sizeof(uint32_t);
+
+	DX::ThrowIfFailed(device->CreateTexture2D(&txtDesc, &initData, m_text.ReleaseAndGetAddressOf()));
+	DX::ThrowIfFailed(device->CreateShaderResourceView(m_text.Get(), nullptr, m_textView.ReleaseAndGetAddressOf()));
+
+	D3D11_SAMPLER_DESC samplerDesc = {};
+	samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+	samplerDesc.MinLOD = 0;
+	samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
+
+	DX::ThrowIfFailed(
+		device->CreateSamplerState(&samplerDesc,
+			m_sampler.ReleaseAndGetAddressOf()));
+}
+
+ID3D11SamplerState* Model::GetTextureSampler()
+{
+	return m_sampler.Get();
+}
+
+ID3D11ShaderResourceView* Model::GetTextureView()
+{
+	return m_textView.Get();
 }
--- textures/Model.h
+++ textures/Model.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include <string>
 
+#include <d3d11.h>
+
 struct Position
 {
 	float X;
@@ -33,6 +35,22 @@ struct TextCoord
 	float Y;
 };
 
+struct Material
+{
+	Material() { ZeroMemory(this, sizeof(Material)); }
+
+	float Ambient[4];
+	float Diffuse[4];
+	float Specular[4]; // w = SpecPower
+	float Reflect[4];
+};
+
+struct SurfaceParams
+{
+	Material Mat;
+	bool HasTexture = false;
+};
+
 class Model
 {
 public:
@@ -41,13 +59,17 @@ public:
 		std::vector<Position>&& positions, 
 		std::vector<Face>&& faces, 
 		std::vector<Normal>&& normals,
-		std::vector<TextCoord>&& textCoords);
+		std::vector<TextCoord>&& textCoords,
+		const char* textPath);
 	static std::unique_ptr<Model> LoadModel(const std::string& filepath);
 
 	const std::vector<Position>& GetPositions() const;
 	const std::vector<Face>& GetFaces() const;
 	const std::vector<Normal>& GetNormals() const;
 	const std::vector<TextCoord>& GetTextCoords() const;
+	void LoadTexture(ID3D11Device* device);
+	ID3D11SamplerState* GetTextureSampler();
+	ID3D11ShaderResourceView* GetTextureView();
 
 private:
 
@@ -55,5 +77,12 @@ private:
 	std::vector<Face>			m_faces;
 	std::vector<Normal>			m_normals;
 	std::vector<TextCoord>		m_textCoords;
+	std::string					m_textPath;
+
+	// texture related
+	Microsoft::WRL::ComPtr<ID3D11Texture2D>				m_text;
+	Microsoft::WRL::ComPtr<ID3D11SamplerState>			m_sampler;
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>	m_textView;
+
 };
 
